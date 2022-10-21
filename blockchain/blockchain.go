00f3e2e238ac5f72abe217e012fc9e0912d4cb12
haimd <haimd@viettelpost.com.vn>
2022-10-21 08:17:29 +0700

    Build with DB

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -3,17 +3,49 @@ package blockchain
 import (
 	b "go-block-chain/block"
 	p "go-block-chain/proof"
+
+	"github.com/boltdb/bolt"
 )
 
+const dbFile = "blockchain.db"
+const blocksBucket = "blocks"
+
 type BlockChain struct {
-	Blocks []*b.Block
+	tip []byte
+	db  *bolt.DB
 }
 
 func NewGenesisBlock() *b.Block {
 	return p.NewBlock("Genesis Block", []byte{})
 }
 func NewBlockChain() *BlockChain {
-	return &BlockChain{[]*b.Block{NewGenesisBlock()}}
+	var tip []byte
+	db, err := bolt.Open(dbFile, 0600, nil)
+	if err != nil {
+		return nil
+	}
+	err = db.Update(func(tx *bolt.Tx) error {
+		b := tx.Bucket([]byte(blocksBucket))
+
+		if b == nil {
+			genesis := NewGenesisBlock()
+			b, err := tx.CreateBucket([]byte(blocksBucket))
+			err = b.Put(genesis.Hash, genesis.Serialize())
+			err = b.Put([]byte("l"), genesis.Hash)
+			if err != nil {
+				return nil
+			}
+			tip = genesis.Hash
+		} else {
+			tip = b.Get([]byte("l"))
+		}
+
+		return nil
+	})
+
+	bc := BlockChain{tip, db}
+
+	return &bc
 }
 func (bc *BlockChain) AddBlock(data string) {
 	prevBlock := bc.Blocks[len(bc.Blocks)-1]
--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module go-block-chain
 
 go 1.16
+
+require (
+	github.com/boltdb/bolt v1.3.1 // indirect
+	golang.org/x/sys v0.1.0 // indirect
+)
